5a2126483af27c4558d616d441ebff288ddef2ed
ESLAM-KU <eslam11222002@gmail.com>
2025-07-09 03:11:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -157,5 +157,6 @@ def chat():
 
 # مناسب لـ Railway
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))  # خلي القيمة الافتراضية 8080
     app.run(host='0.0.0.0', port=port)
+
